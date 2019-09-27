b20aca5068954416f85818edd517b1660dfa9f5a
Sergio <sergio@filhote.local>
2019-09-27 18:32:45 -0300

    Demo Liquibase

---
+++ demo_liquibase/sqlsaturdaysp906/src/main/java/com/example/sqlsaturdaysp906/Sqlsaturdaysp906Application.java
@@ -0,0 +1,13 @@
+package com.example.sqlsaturdaysp906;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sqlsaturdaysp906Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sqlsaturdaysp906Application.class, args);
+	}
+
+}
